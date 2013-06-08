0341129c191eca34c684b2f5d552908d8454b721
trarck <trarck@gmail.com>
2013-06-08 21:59:20 +0800

    加入模块操作

---
+++ YHScript/Module.h
@@ -0,0 +1,32 @@
+//
+//  ScriptCore.h
+//  testmonkey
+//
+//  Created by Rolando Abarca on 3/14/12.
+//  Copyright (c) 2012 Zynga Inc. All rights reserved.
+//
+
+#ifndef YHSCRIPT_JSVALUTIL_H_
+#define YHSCRIPT_JSVALUTIL_H_
+
+#include "cocos2d.h"
+#include "jsapi.h"
+#include "YHScriptMacros.h"
+
+NS_YHSCRIPT_BEGIN
+
+class Module
+{
+public:
+
+    Module();
+    ~Module();
+    bool init();
+
+    void load();
+
+};
+
+NS_YHSCRIPT_END
+
+#endif //YHSCRIPT_JSVALUTIL_H_
